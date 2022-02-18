dfacf46d0eecfe6bdbb9b95a4b7b33beaa236229
Rogelio Gamboa Jr <roggam@yahoo.com.mx>
2022-02-18 12:23:49 -0500

    first commit

---
+++ src/main/java/com/in28minutes/fullstack/springboot/maven/crud/springbootfullstackcrudfullstackwithmaven/SpringReactAppApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.fullstack.springboot.maven.crud.springbootfullstackcrudfullstackwithmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactAppApplication.class, args);
+	}
+
+}
